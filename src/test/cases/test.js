ada0a1ba7151f512ddc2ffaca2a3ac970aa1c9e7
Tyler Burkett <tyler.burkett@uky.edu>
2019-11-21 16:57:03 -0500

    added new path in Express app

--- src/test/cases/test.js
+++ src/test/cases/test.js
@@ -15,4 +15,14 @@ describe('GET - /', async () => {
         expect(response.text).to.equal('Hello World!')
     })
     
+    it('returns 200 on /2 path', async () => {
+        // make a GET request on the app for the url '/'
+        const response = await request(app).get('/2')
+
+        // check the HTTP status code
+        expect(response.statusCode).to.equal(200)
+        // check the raw text returned
+        expect(response.text).to.equal('Hello World 2!')
+    })
+    
 })
